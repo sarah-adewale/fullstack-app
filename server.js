d55eb8f5f6ad4dd50279a53f12b277a4020270ec
Sarah Adewale <saraphynaadewale@gmail.com>
2022-06-23 01:21:39 +0100

    modified gitignore

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongobd').MongoClient
 require('dotenv').config()
+const PORT = 2000
+
+app.use(cors())
+
+
+
 
 let db,
     dbConnectionString = process.env.DB_STRING, //
-    dbName = '',
-    collection = 
+    dbName = 'fullstack-app',
+    collection 
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('connected to database')
+        db = client.db(dbName)
+        collection = db.collection('fullstack-app-obj')
+    })
