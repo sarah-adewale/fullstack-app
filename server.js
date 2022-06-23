3f2d8f9e16c936cb82fee54ffc8cd65962a3055a
Sarah Adewale <saraphynaadewale@gmail.com>
2022-06-23 01:07:55 +0100

    added mongodb connection

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongobd').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING, //
+    dbName = '',
+    collection = 
